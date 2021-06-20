2a87eb3889ba94743d864271449f2973b14ffa7f
Ardian Effendi <aeff0002@student.monash.edu>
2021-06-20 11:23:05 +0800

    changes on SplashScreen Title is shown first followed by another subtitle in which animation is fading in bottom-up. Wooden music instrument has shaking animation.

--- KayuhBaimbai/app/src/main/java/com/blogspot/wasakamantap/SplashScreen.kt
+++ KayuhBaimbai/app/src/main/java/com/blogspot/wasakamantap/SplashScreen.kt
@@ -5,6 +5,7 @@ import android.media.MediaPlayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.animation.Animation
@@ -24,6 +25,9 @@ class SplashScreen : AppCompatActivity() {
     // Animation Variables
     private lateinit var topAnim: Animation
     private lateinit var bottomAnim: Animation
+    private lateinit var fadeAnim: Animation
+    private lateinit var lateBottomAnim: Animation
+    private lateinit var shakyAnimation: Animation
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +37,9 @@ class SplashScreen : AppCompatActivity() {
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        lateBottomAnim = AnimationUtils.loadAnimation(this, R.anim.late_bottom_animation)
+        fadeAnim = AnimationUtils.loadAnimation(this, R.anim.fading_animation)
+        shakyAnimation = AnimationUtils.loadAnimation(this, R.anim.shaky_animation)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -43,18 +50,31 @@ class SplashScreen : AppCompatActivity() {
             )
         }
 
-        // Set the views to its respective animation
-        binding.ivKayuh.animation = topAnim
-        binding.ivAcc.animation = topAnim
-        binding.ivBaimbai.animation = bottomAnim
-
         // Coroutines in activity scope
         activityScope.launch {
             // Create media player to play audio during splash screen
             val media: MediaPlayer = MediaPlayer.create(this@SplashScreen, R.raw.panting_sound)
             media.start()
-            delay(5000)
+
+            // animation for main part
+            binding.apply {
+                ivKayuh.animation = topAnim
+                ivAcc.animation = topAnim
+                ivBaimbai.animation = bottomAnim
+                ivWood.animation = shakyAnimation
+            }
+            delay(4000)
+
+            // animation after half duration of splash screen
+            binding.apply {
+                ivRakat.visibility = View.VISIBLE
+                ivLeaves.visibility = View.VISIBLE
+                ivLeaves.animation = fadeAnim
+                ivRakat.animation = lateBottomAnim
+            }
+            delay(4000)
             media.pause()
+
             // Intent to Main Activity
             val intent = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(intent)
